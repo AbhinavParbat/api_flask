c49626d6ca616aabd312675346fbec461129ad83
Abhinav Kumar Parbat <abhinavparbat@gmail.com>
2026-02-08 12:03:27 +0100

    After adding method - POST

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ def get_user(user_id):
         user_data["extra"] = extra
 
     return jsonify(user_data), 200
+
+@app.route("/create-user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    return jsonify (data), 201
+
 if __name__ == "__main__":
     app.run(debug=True)
 
